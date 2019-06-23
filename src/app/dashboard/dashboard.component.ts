01eb5b6a546b58015d4fa65bd437c1b823a40b4b
Anne Morel <stephmorel8910@gmail.com>
2019-06-23 13:58:29 -0400

    idek

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from  '../auth/auth.service';
 import { StorageService, UserData } from '../storage/storage.service'
+import { Router } from  "@angular/router";
 
 
 @Component({
@@ -10,12 +11,15 @@ import { StorageService, UserData } from '../storage/storage.service'
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public authService:  AuthService, public storageService: StorageService, public userData: UserData) { }
+  constructor(public authService:  AuthService, public storageService: StorageService, public userData: UserData, public router: Router) { }
 
   ngOnInit() {
     this.storageService.userInfoObservable.subscribe((userDataTemp: UserData) => {
       console.log(userDataTemp);
       this.userData=userDataTemp;
+      if(!this.userData.isFullyLoggedIn) {
+        this.router.navigate(['registration-form']);
+      }
     });
   }
 
